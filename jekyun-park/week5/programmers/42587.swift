d105563d668c6474bc501cb2d6bec9010e624369
jegyun <parkjekyun@gmail.com>
2022-01-31 18:50:31 +0900

    Update 42587.swift

--- jekyun-park/week5/programmers/42587.swift
+++ jekyun-park/week5/programmers/42587.swift
@@ -9,28 +9,27 @@ import Foundation
 
 func solution(_ priorities: [Int], _ location: Int) -> Int {
 
-    // key : 문서 index , value : 우선순위
-    var dictionary: [Int: Int] = [:]
+    // (index , priority)
+    var queue: [(Int, Int)] = []
 
     for i in 0..<priorities.count {
-        dictionary[i] = priorities[i]
+        queue.append((i, priorities[i]))
     }
 
-    var stack = Array(dictionary)
-    var printed: [(key:Int,value:Int)] = []
-    stack.sort(by: <)
-    
-    while !stack.isEmpty {
-        let (index,priority) = stack.removeFirst()
-        
-        if stack.contains(where: { $0.value > priority }) == true {
-            stack.append((key:index,value:priority))
+    var printed: [(Int, Int)] = []
+
+    while !queue.isEmpty {
+        let (index, priority) = queue.removeFirst()
+
+        if queue.contains(where: { $0.1 > priority }) == true {
+            queue.append((index, priority))
         } else {
-            printed.append((key:index,value:priority))
+            printed.append((index, priority))
         }
     }
-    return printed.firstIndex { $0.key == location }! + 1
+    
+    return printed.firstIndex { $0.0 == location }! + 1
 }
 
-print(solution([2, 1, 3, 2], 2)) // 1
-print(solution([1, 1, 9, 1, 1, 1], 0)) // 5
+// print(solution([2, 1, 3, 2], 2)) // 1
+// print(solution([1, 1, 9, 1, 1, 1], 0)) // 5
